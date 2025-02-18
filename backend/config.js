badf48792cf4b0efb9c325b195ff444855d13d76
profTanish <profile.tanish@gmail.com>
2025-02-18 14:11:43 +0530

    Created Basic Frontend Backend of payment web-app

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+//backend/config.js
+module.exports = {
+	JWT_SECRET: "Created_by_Tanish",
+	MONGO_URL: "Your Mongo Url"
+}
---
+++ db.js
@@ -0,0 +1,3 @@
+// const { MONGO_URL } = require('./backend/config');
+// // ... rest of your database connection code
+// mongoose.connect(MONGO_URL) 
