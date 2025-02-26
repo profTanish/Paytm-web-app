dbfa396e6302655ecc2cbbf0bd9b80fa27795b98
profTanish <profile.tanish@gmail.com>
2025-02-26 05:31:21 +0530

    versel.json

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,11 @@ const rootRouter = require("./routes/index");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://paytm-web-app-host.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
